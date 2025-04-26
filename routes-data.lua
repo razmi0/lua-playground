01d7b288135d6854ea231cbfef4cced194f06d46
razmiO <thomas.cuesta31@gmail.com>
2025-04-26 16:03:00 +0200

    refactor(baseTests): base features tests dedicated function

--- routes-data.lua
+++ routes-data.lua
@@ -2,135 +2,204 @@ local Trie = require("trie-router")
 local inspect = require("inspect")
 local trie = Trie.new()
 
+local baseTests = function(middlewares, from, to)
+    local routes = {
+        {
+            route = { path = "*", handler = function() return "wildcard" end },
+            requested = {
+                { path = "/yooo", expectedResult = "wildcard" }
+            }
+        },
+        {
+            route = { path = "/", handler = function() return "Racine" end },
+            requested = {
+                { path = "/", expectedResult = "Racine" }
+            }
+        },
+        {
+            route = { path = "/data", handler = function() return "Données" end },
+            requested = {
+                { path = "/data", expectedResult = "Données" }
+            }
+        },
+        {
+            route = { path = "/info", handler = function() return "Informations" end },
+            requested = {
+                { path = "/info", expectedResult = "Informations" }
+            }
+        },
+        {
+            route = {
+                path = "/users/:new{%a+}",
+                handler = function(params)
+                    return "Utilisateur: " ..
+                        params.new
+                end
+            },
+            requested = {
+                {
+                    path = "/users/alice", expectedResult = "Utilisateur: alice"
+                }
+            }
+        },
+        {
+            route = {
+                path = "/items/:id{%d+}", handler = function(params) return "Item ID: " .. params.id end
+            },
+            requested = {
+                { path = "/items/123", expectedResult = "Item ID: 123" }
+            }
+        },
+        {
+            route = {
+                path = "/products/:category?",
+                handler = function(params)
+                    if not next(params) then return "Tous les produits" end
+                    return "Catégorie: " ..
+                        params.category
+                end
+            },
+            requested = {
+                {
+
+                    path = "/products/laptop",
+                    expectedResult = "Catégorie: laptop"
+                },
+                {
+
+                    path = "/products",
+                    expectedResult = "Tous les produits"
+                }
+            }
+        },
+        {
+            route = {
+
+                path = "/articles/:page?{%d+}",
+                handler = function(params)
+                    if not next(params) then
+                        return "Première page des articles"
+                    end
+                    return "Page: " .. params.page
+                end
+            },
+            requested = {
+                { path = "/articles",   expectedResult = "Première page des articles" },
+                { path = "/articles/5", expectedResult = "Page: 5" }
+            }
+        },
+        {
+            route = {
+
+                path = "/search/:query?/results",
+                handler = function(params)
+                    if not next(params) then
+                        return "Résultats de la recherche"
+                    end
+                    return "Recherche: " ..
+                        params.query .. ", Résultats"
+                end
+            },
+            requested = {
+                { path = "/search/results", expectedResult = "Résultats de la recherche" },
+                {
+
+                    path = "/search/lua/results",
+                    expectedResult = "Recherche: lua, Résultats"
+                }
+            }
+        },
+        {
+            route = {
+
+                path = "/config/:type?/:key?",
+                handler = function(params)
+                    if not next(params) then
+                        return "Pas de config"
+                    end
+                    return "Config " .. (params.type or "no-type") .. " and " .. (params.key or "no-key")
+                end
+            },
+            requested = {
+                { path = "/config",           expectedResult = "Pas de config" },
+                { path = "/config/user",      expectedResult = "Config user and no-key" },
+                { path = "/config/user/item", expectedResult = "Config user and item" }
+            }
+        },
+        {
+            route = {
+
+                path = "/tables/:type?/row/:col?",
+                handler = function(params)
+                    if not next(params) then
+                        return "Pas de tables"
+                    end
+                    return "Tables " .. (params.type or "no-type") .. " and " .. (params.col or "no-col")
+                end
+            },
+            requested = {
+                { path = "/tables/users/row/2", expectedResult = "Tables users and 2" },
+                { path = "/tables/users/row",   expectedResult = "Tables users and no-col" },
+            }
+        },
 
 
-
-local routes = {
-    { method = "GET", path = "*",                handler = function() return "wildcard" end },
-    { method = "GET", path = "/",                handler = function() return "Racine" end },
-    { method = "GET", path = "/data",            handler = function() return "Données" end },
-    { method = "GET", path = "/info",            handler = function() return "Informations" end },
-
-    { method = "GET", path = "/users/:new{%a+}", handler = function(params) return "Utilisateur: " .. params.new end },
-    { method = "GET", path = "/items/:id{%d+}",  handler = function(params) return "Item ID: " .. params.id end },
-    {
-        method = "GET",
-        path = "/products/:category?",
-        handler = function(params)
-            if not next(params) then return "Tous les produits" end
-            return "Catégorie: " ..
-                params.category
-        end
-    },
-
-    {
-        method = "GET",
-        path = "/articles/:page?{%d+}",
-        handler = function(params)
-            if not next(params) then
-                return "Première page des articles"
+    }
+    local method = "GET"
+    from = from or 0
+    to = to or #routes
+    for score, route in ipairs(routes) do
+        if score < from or score > to then goto continue end
+        local registered_route = route.route
+        if middlewares then
+            for _, mw in ipairs(middlewares) do
+                if mw.score == score then
+                    trie:insert("USE", registered_route.path, registered_route.handler)
+                end
             end
-            return "Page: " .. params.page
         end
-    },
-    {
-        method = "GET",
-        path = "/search/:query?/results",
-        handler = function(params)
-            if not next(params) then
-                return "Résultats de la recherche"
-            end
-            return "Recherche: " ..
-                params.query .. ", Résultats"
-        end
-    },
-    {
-        method = "GET",
-        path = "/config/:type?/:key?",
-        handler = function(params)
-            if not next(params) then
-                return "Pas de config"
-            end
-            return "Config " .. (params.type or "no-type") .. " and " .. (params.key or "no-key")
-        end
-    }, {
-
-    method = "GET",
-    path = "/tables/:type?/row/:col?",
-    handler = function(params)
-        if not next(params) then
-            return "Pas de tables"
-        end
-        return "Tables " .. (params.type or "no-type") .. " and " .. (params.col or "no-col")
+        trie:insert(method, registered_route.path, registered_route.handler)
+        ::continue::
     end
-}
-
-}
-
-local requested_routes = {
-    { method = "GET", path = "/",            expectedResult = "Racine" },
-    { method = "GET", path = "/yooo",        expectedResult = "wildcard" },
-    { method = "GET", path = "/data",        expectedResult = "Données" },
-    { method = "GET", path = "/info",        expectedResult = "Informations" },
-
-    { method = "GET", path = "/users/alice", expectedResult = "Utilisateur: alice" },
-    { method = "GET", path = "/items/123",   expectedResult = "Item ID: 123" },
-    {
-        method = "GET",
-        path = "/products/laptop",
-        expectedResult = "Catégorie: laptop"
-    },
-
-    { method = "GET", path = "/articles/5",         expectedResult = "Page: 5" },
-    {
-        method = "GET",
-        path = "/search/lua/results",
-        expectedResult = "Recherche: lua, Résultats"
-    },
-
-    { method = "GET", path = "/products",           expectedResult = "Tous les produits" },
-    { method = "GET", path = "/articles",           expectedResult = "Première page des articles" },
-    { method = "GET", path = "/search/results",     expectedResult = "Résultats de la recherche" },
-
-    { method = "GET", path = "/config",             expectedResult = "Pas de config" },
-    { method = "GET", path = "/config/user",        expectedResult = "Config user and no-key" },
-    { method = "GET", path = "/config/user/item",   expectedResult = "Config user and item" },
-    { method = "GET", path = "/tables/users/row/2", expectedResult = "Tables users and 2" },
-    { method = "GET", path = "/tables/users/row",   expectedResult = "Tables users and no-col" },
-
-}
 
 
 
--- trie:insert("USE", "*", function() return "USE" .. " : " .. "1" end)
+    local results = {
+        success = {},
+        failed = {}
 
-for _, route in ipairs(routes) do
-    trie:insert(route.method, route.path, route.handler)
+    }
+
+    for score, route in ipairs(routes) do
+        local requested_routes = route.requested
+        local registered_route = route.route
+
+        if score < from or score > to then goto continue end
+        for _, requested in ipairs(requested_routes) do
+            local actualResult = {}
+            local handlers, params = trie:search(method, requested.path)
+            if handlers then
+                for _, h in ipairs(handlers) do
+                    table.insert(actualResult, h(params))
+                end
+            end
+            local success = (actualResult[#actualResult] == requested.expectedResult)
+            table.insert(results[((success and "success") or "failed")], {
+                req_path = requested.path or nil,
+                route = registered_route.path or nil,
+                expected = requested.expectedResult or false,
+                actual = actualResult or nil,
+                mw_attached = #actualResult - 1,
+            })
+        end
+        ::continue::
+    end
+
+    if next(results.failed) then
+        print("[ERROR] : BASE TEST")
+        print(inspect(results.failed))
+    end
 end
 
--- print(inspect(trie()))
 
-local results = {
-    success = {},
-    failed = {}
-
-}
-for _, route in ipairs(requested_routes) do
-    local handlers, params = trie:search(route.method, route.path)
-
-    local actualResult = nil
-    if handlers then
-        actualResult = handlers(params)
-    end
-
-    local success = (actualResult == route.expectedResult)
-    table.insert(results[((success and "success") or "failed")], {
-        path = route.path or "nil",
-        expected = route.expectedResult or "nil",
-        actual = actualResult or "nil",
-    })
-end
-
-print(inspect(results))
--- print(inspect(trie()))
--- /config/:1/:2 : /config/user => not found
+baseTests()
