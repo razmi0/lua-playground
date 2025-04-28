162e98e687e95542c8ad744bad3473e5bcb363dc
razmiO <thomas.cuesta31@gmail.com>
2025-04-29 01:29:32 +0200

    *

--- utils/tx.lua
+++ utils/tx.lua
@@ -1,5 +1,11 @@
 local inspect = require("inspect")
 
+local Tx = {}
+
+local fails_count = 0
+local tests_count = 0
+local name = ""
+
 local function c(color, str)
     local colors = {
         red = "\27[31m",
@@ -10,25 +16,7 @@ local function c(color, str)
     return code .. str .. colors.reset
 end
 
-local Tx = {}
 
-local fails_count = 0
-local tests_count = 0
-local name = ""
-
-
-function Tx.describe(xname, fn)
-    fails_count = 0
-    tests_count = 0
-    name = xname
-    print("\n" .. name)
-    fn()
-    if fails_count == 0 then
-        print(c("green", " ok"))
-    else
-        print(c("reset", "Failed ") .. c("red", tostring(fails_count)) .. "/" .. tostring(tests_count))
-    end
-end
 
 local function deep_contains(container, value)
     if type(container) == "string" then
@@ -46,21 +34,18 @@ local function deep_contains(container, value)
     return false
 end
 
-function Tx.not_include(container, value)
-    if deep_contains(container, value) then
-        error(c("red", "Did not expect to find " .. tostring(value)))
-    end
-end
 
-function Tx.throws(fn)
-    local ok, _ = pcall(fn)
-    if ok then
-        error(c('red', "Expected error to be thrown, but none was"))
+function Tx.describe(xname, fn)
+    fails_count = 0
+    tests_count = 0
+    name = xname
+    print("\n" .. name)
+    fn()
+    if fails_count == 0 then
+        print(c("green", " ok"))
+    else
+        print(c("reset", "Failed ") .. c("red", tostring(fails_count)) .. "/" .. tostring(tests_count))
     end
-end
-
-local function space(n)
-    return string.rep("\n\t", n)
 end
 
 function Tx.it(err, func)
@@ -109,7 +94,7 @@ function Tx.equal(actual, expected)
     end
 
     if not deep_equal(actual, expected) then
-        error(c("red", space(1) .. inspect(actual) .. " ~= " .. inspect(expected)))
+        error(c("red", inspect(actual) .. " ~= " .. inspect(expected)))
     end
 end
 
@@ -119,9 +104,22 @@ function Tx.include(container, value)
     end
 end
 
+function Tx.not_include(container, value)
+    if deep_contains(container, value) then
+        error(c("red", "Did not expect to find " .. tostring(value)))
+    end
+end
+
 function Tx.contain(string_val, substring)
     if not string.find(string_val, substring, 1, true) then
         error(c("red", ("'" .. string_val .. "' !~ '" .. substring .. "'")))
+    end
+end
+
+function Tx.throws(fn)
+    local ok, _ = pcall(fn)
+    if ok then
+        error(c('red', "Expected error to be thrown, but none was"))
     end
 end
 
