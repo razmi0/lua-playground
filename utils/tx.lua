ce576826f9f7cf014c4d383fca79d781b5190cde
razmiO <thomas.cuesta31@gmail.com>
2025-04-29 01:09:15 +0200

    feat: more methods

--- utils/tx.lua
+++ utils/tx.lua
@@ -26,7 +26,36 @@ function Tx.describe(xname, fn)
     if fails_count == 0 then
         print(c("green", " ok"))
     else
-        print(c("red", "Failed " .. tostring(fails_count)) .. "/" .. tostring(tests_count))
+        print(c("reset", "Failed ") .. c("red", tostring(fails_count)) .. "/" .. tostring(tests_count))
+    end
+end
+
+local function deep_contains(container, value)
+    if type(container) == "string" then
+        return container:find(value, 1, true) ~= nil
+    elseif type(container) == "table" then
+        for _, v in pairs(container) do
+            if v == value then
+                return true
+            end
+            if type(v) == "table" and deep_contains(v, value) then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+function Tx.not_include(container, value)
+    if deep_contains(container, value) then
+        error(c("red", "Did not expect to find " .. tostring(value)))
+    end
+end
+
+function Tx.throws(fn)
+    local ok, _ = pcall(fn)
+    if ok then
+        error(c('red', "Expected error to be thrown, but none was"))
     end
 end
 
@@ -43,7 +72,7 @@ function Tx.it(err, func)
     if not success then
         fails_count = fails_count + 1
         io.write(c("red", "+ "))
-        print(c("red", "\n" .. err), space(1), c("red", internal_err_msg))
+        print(c("red", "\n" .. err), c("red", internal_err_msg))
     else
         io.write(c("green", "+"))
     end
@@ -84,16 +113,9 @@ function Tx.equal(actual, expected)
     end
 end
 
-function Tx.include(list, value)
-    local found = false
-    for _, item in ipairs(list) do
-        if item == value then
-            found = true
-            break
-        end
-    end
-    if not found then
-        error(c("red", ("missing " .. tostring(value))))
+function Tx.include(container, value)
+    if not deep_contains(container, value) then
+        error(c("red", "Did not expect to find " .. tostring(value)))
     end
 end
 
@@ -101,6 +123,10 @@ function Tx.contain(string_val, substring)
     if not string.find(string_val, substring, 1, true) then
         error(c("red", ("'" .. string_val .. "' !~ '" .. substring .. "'")))
     end
+end
+
+function Tx.fail(msg)
+    error(msg or "Forced failure")
 end
 
 return Tx
