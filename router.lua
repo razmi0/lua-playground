c041d7a344b2771b9cda062c93ed413e79303cd1
razmiO <thomas.cuesta31@gmail.com>
2025-05-01 14:49:36 +0200

    feat: add router facade for trie router

--- router.lua
+++ router.lua
@@ -1,11 +1,21 @@
+local Trie = require("trie-router")
+
 local Router = {}
 Router.__index = Router
-Router.__name = "Router"
+Router.__name = "TrieRouter"
+
 function Router.new()
     return setmetatable({
+        trie = Trie.new()
     }, Router)
 end
 
-return {
+function Router:add(method, path, handlers)
+    self.trie:insert(method, path, handlers)
+end
 
-}
+function Router:match(method, path)
+    return self.trie:search(method, path)
+end
+
+return Router
