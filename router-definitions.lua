cffd45d24ec0c85b464b00f37f1c73e42dde0baf
razmiO <thomas.cuesta31@gmail.com>
2025-05-02 11:58:57 +0200

    refactor: Set initialisation at the top of the file

--- router-definitions.lua
+++ router-definitions.lua
@@ -1,14 +1,18 @@
 local Set = require("utils.set")
+local inspect = require("inspect")
 
 local STD_METHODS = { "GET", "POST", "PUT", "PATCH", "HEAD", "OPTIONS", "DELETE" }
 local MW_METHOD = "USE"
 local ALL_METHOD = "ALL"
 
+local STD_SET = Set.new():add(STD_METHODS)
+local SET_ALL_AVAILABLE_METHODS = Set.new():add(STD_METHODS):add(MW_METHOD):add(ALL_METHOD)
+
 return {
     methods = {
-        STD_METHODS = Set.new():add(STD_METHODS),
+        STD_METHODS = STD_SET,
         MW_METHOD = MW_METHOD,
         ALL_METHOD = ALL_METHOD,
-        ALL_AVAILABLE_METHODS = Set.new():add(STD_METHODS):add(MW_METHOD):add(ALL_METHOD)
+        ALL_AVAILABLE_METHODS = SET_ALL_AVAILABLE_METHODS
     },
 }
