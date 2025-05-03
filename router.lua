c183b42ac6f805d510f15368aca3dfa04f41b999
razmiO <thomas.cuesta31@gmail.com>
2025-05-03 19:52:21 +0200

    feat: add route method guards

--- router.lua
+++ router.lua
@@ -1,8 +1,12 @@
-local Trie = require("trie-router")
+local Trie                  = require("trie-router")
 
-local Router = {}
-Router.__index = Router
-Router.__name = "TrieRouter"
+local Router                = {}
+Router.__index              = Router
+Router.__name               = "TrieRouter"
+local defs                  = require("router-definitions")
+local ALL_AVAILABLE_METHODS = defs.methods.ALL_AVAILABLE_METHODS
+local ALL_METHOD            = defs.methods.ALL_METHOD
+local STD_METHODS           = defs.methods.STD_METHODS
 
 function Router.new()
     return setmetatable({
@@ -10,8 +14,20 @@ function Router.new()
     }, Router)
 end
 
-function Router:add(method, path, handlers)
-    self.trie:insert(method, path, handlers)
+function Router:add(method, path, ...)
+    if not ALL_AVAILABLE_METHODS:has(method) then
+        error("Unknown method : " .. method)
+        return
+    end
+
+    if method == ALL_METHOD then
+        for _, m in ipairs(STD_METHODS:entries()) do
+            self.trie:insert(m, path, ...)
+        end
+        return
+    end
+
+    self.trie:insert(method, path, ...)
 end
 
 function Router:match(method, path)
